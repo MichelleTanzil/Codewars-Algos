76f9da3c18f8204d1411d7a8b00eaf517bae1bff
Michelle Tanzil <mtanzil01@gmail.com>
2020-03-22 08:09:15 -0700

    completed counting duplicates algo

---
+++ counting_duplicates/counting_duplicates.cs
@@ -0,0 +1,43 @@
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+public class Kata
+{
+  public static int DuplicateCount(string str)
+  {
+    str = str.ToLower();
+    Dictionary<char, int> hashMap = new Dictionary<char, int>();
+    int duplicates = 0;
+
+    for(int i=0; i < str.Length; i++){
+      if(hashMap.ContainsKey(str[i])) hashMap[str[i]]++;
+      else hashMap[str[i]] = 1;
+    }
+    foreach(var item in hashMap)
+      if(item.Value > 1) duplicates++;
+    return duplicates;
+  }
+}
+
+// Optimized Solution
+public class Kata
+{
+  public static int DuplicateCount(string str)
+  {
+    return str.ToLower().GroupBy(c => c).Where(g => g.Count() > 1).Count();
+  }
+}
